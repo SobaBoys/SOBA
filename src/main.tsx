186f867daa9b8a810c72a70e0fbd8b6bae61468d
Jae Han Song <rfv1479@gmail.com>
2023-03-09 21:40:17 +0900

    SB-24 feat : index.html max-width 및 font 추추가
    - index.html 추가
    - font.css를 활용하여 폰트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './styles/global.css';
+import './styles/font.css';
 
 ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
   <React.StrictMode>
