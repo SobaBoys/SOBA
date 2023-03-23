99159d8ec241a0798d9e5ebeb4fdd726d0224b22
turfguy <ksm0811@ajou.ac.kr>
2023-03-23 20:11:46 +0900

    SB-13 chore: CI/CD test

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -3,7 +3,7 @@ export default function MainPage() {
     <main className="flex flex-col text-center h-full items-center justify-around">
       <h1 className="font-bold">Dev-Calendar</h1>
       <div className="w-full flex flex-col gap-4">
-        <button>버튼</button>
+        <button>Button</button>
       </div>
     </main>
   );
